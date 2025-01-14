ed8fb52d7ab8b8426e3d1266c1b832136b81707a
marki32 <idlumino175@gmail.com>
2025-01-14 08:25:03 +0530

    Initial commit

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Download yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+# Make the script executable even if Windows changed line endings
+if [ "$(uname)" == "Linux" ]; then
+  # Only run these commands on Linux (Vercel)
+  
+  # Download yt-dlp
+  curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 
-# Make it executable
-chmod a+rx yt-dlp
+  # Make it executable
+  chmod a+rx yt-dlp
 
-# Move to a directory in PATH
-mkdir -p .vercel/bin
-mv yt-dlp .vercel/bin/
+  # Create bin directory and move yt-dlp
+  mkdir -p .vercel/bin
+  mv yt-dlp .vercel/bin/
 
-# Install ffmpeg
-apt-get update && apt-get install -y ffmpeg
+  # Install ffmpeg
+  apt-get update && apt-get install -y ffmpeg
+fi
+
+# Run next build
+next build
